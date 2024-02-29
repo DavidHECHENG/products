70ece622ecb0b4d507392f2ba19d57da7538c4eb
HE CHENG <as0960311360@gmail.com>
2024-02-29 17:02:29 +0200

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
 products = []
+
+# 讀取檔案
+with open('products.csv','r',encoding= 'utf-8') as f :
+    for line in f :
+        if '商品,價格' in line :
+            continue
+        name , price = line.strip().split(',')
+        products.append([name,price])
+
+# 讓使用者輸入
 while True :
     name = input('請輸入商品名稱：')
     if name == 'q' :
@@ -6,9 +16,11 @@ while True :
     price = input('請輸入商品價格：')
     products.append([name,price])
 
+# 印出購買紀錄
 for p in products :
     print(p[0],'的價格為',p[1],'元')
 
+# 寫入檔案
 with open('products.csv','w',encoding= 'utf-8') as f :
     f.write('商品,價格\n')
     for p in products :
